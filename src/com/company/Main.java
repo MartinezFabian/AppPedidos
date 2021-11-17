15cc0c66f61ba8fb330b1f7a6abdfead4b32d44c
MartinezFabian <FabianMartinezDev@gmail.com>
2021-11-17 20:42:04 -0300

    Se agregaron mejoras

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new VentanaPedidos();
+        frame.setLocationRelativeTo(null); //frame inicializara en el centro de pantalla
 	}
 }
